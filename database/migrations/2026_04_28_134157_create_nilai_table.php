1069a1c3a1643d60a707e71a1ce29b2c34762cfd
qoisnizar123 <nizarmahendra919@gmail.com>
2026-05-03 12:30:12 +0700

    first commit

---
+++ database/migrations/2026_04_28_134157_create_nilai_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nilai', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mahasiswa_id')->constrained('mahasiswa')->onDelete('cascade');
+            $table->foreignId('mata_kuliah_id')->constrained('mata_kuliah')->onDelete('cascade');
+            $table->decimal('nilai_angka', 5, 2);
+            $table->string('nilai_huruf');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nilai');
+    }
+};
