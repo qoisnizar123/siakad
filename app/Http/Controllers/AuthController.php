71a524b206d4501f774e243f438b6cb72b03972c
Your Name <you@example.com>
2026-05-11 11:29:06 +0700

    update authcontroller and web.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
